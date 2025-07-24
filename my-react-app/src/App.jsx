def563629d58f0e1b8af4775e7169a4b50eda6e0
Chaudharyjayant <chaudharyjayant980@gmail.com>
2025-07-24 19:53:58 +0530

    Initialized a repo for reatjs

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  const name = "Jayant";
+  return(
+    <div>
+      <h1>Hello, {name}!</h1>
+      <p>React Day 1 : Learning the jsx how it can use the variables in html.</p>
+    </div>
+  );
+}
+
+export default App;
