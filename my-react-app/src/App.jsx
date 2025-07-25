7437984dc591fb4c9b5d60da7e89d0fff5dcb8ca
Chaudharyjayant <chaudharyjayant980@gmail.com>
2025-07-25 20:20:50 +0530

    Learnt about the Props

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,9 +1,36 @@
-import Welcome from "./Welcome";
+
+import Profile from "./Profile";
 
 function App() {
   return (
     <div>
-      <Welcome />
+      <h1>Meet our Team</h1>
+
+      <Profile 
+        name= "Jayant chaudhary"
+        age = {20}
+        bio = "Backend Developer Learning react and java"
+        />
+
+      <Profile
+        name = "Akshita Dubey"
+        age = {20}
+        bio = "Intern at dehradun court, Pursuing BBA LLB from Law College Dehradun"
+      
+      />
+
+      <Profile
+        name = "Kartikeya Pathak"
+        age = {21}
+        bio = "Frontend developer enthusiast with interest in UI/UX" 
+      />
+
+      <Profile
+        name = "Himanshu Tyagi"
+        age = {21}
+        bio = "Full stack and Hybrid app developer"
+      />
+
     </div>
   );
 }
