66781a659d8e3a039690db55f377cf8367349d5a
Chaudharyjayant <chaudharyjayant980@gmail.com>
2025-07-26 19:22:50 +0530

    Learn to render List with ,map() function

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -2,37 +2,49 @@
 import Profile from "./Profile";
 
 function App() {
-  return (
-    <div>
-      <h1>Meet our Team</h1>
-
-      <Profile 
-        name= "Jayant chaudhary"
-        age = {20}
-        bio = "Backend Developer Learning react and java"
-        />
-
-      <Profile
-        name = "Akshita Dubey"
-        age = {20}
-        bio = "Intern at dehradun court, Pursuing BBA LLB from Law College Dehradun"
+  const users = [
+    
       
-      />
 
-      <Profile
-        name = "Kartikeya Pathak"
-        age = {21}
-        bio = "Frontend developer enthusiast with interest in UI/UX" 
-      />
+      {
+        name: "Jayant chaudhary",
+        age : 20,
+        bio : "Backend Developer Learning react and java",
+      }, 
+      {
+        name : "Akshita Dubey",
+        age : 20,
+        bio : "Intern at dehradun court, Pursuing BBA LLB from Law College Dehradun",
+      },
+      {
+        name : "Kartikeya Pathak",
+        age : 21,
+        bio : "Frontend developer enthusiast with interest in UI/UX" ,
+      },
 
-      <Profile
-        name = "Himanshu Tyagi"
-        age = {21}
-        bio = "Full stack and Hybrid app developer"
-      />
+      {
+        name : "Himanshu Tyagi",
+        age : 21,
+        bio : "Full stack and Hybrid app developer",
+      },
 
-    </div>
-  );
+
+    ];
+
+    return (
+      <div>
+        <h1>Meet our Team</h1>
+
+        {users.map((user, index) => (
+          <Profile
+            key = {index}
+            name = {user.name}
+            age = {user.age}
+            bio = {user.bio}
+            />
+        ))}
+      </div>
+    );
 }
 
 export default App;
